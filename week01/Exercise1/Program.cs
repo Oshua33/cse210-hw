a11571abf82daa2f5db0269966ccada7468065ee
oshua33 <oshuaaigbor@gmail.com>
2026-05-08 23:35:29 +0100

    inital commit

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        string school = "BYU-Idaho";
+        Console.WriteLine($"I am studying at {school}. ");
+
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        Console.WriteLine(firstName);
+
+        Console.WriteLine("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+
     }
+
 }
